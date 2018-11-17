31c820bce90f6bb2c5bc458a541c9084409e6ae8
sebas8824 <sebastian.benitez@outlook.com>
2018-11-17 18:19:53 -0500

    Fundamentos basicos del uso de Sync, Fetch y Push en una PWA

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
 if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('/sw.js');
+    navigator.serviceWorker.register('/sw.js')
+        .then(reg => {
+            /*setTimeout(() => {
+                reg.sync.register('posteo-programitas');
+                console.log('Se enviaron los archivos al server');
+            }, 3000);*/
+            Notification.requestPermission().then(result => {
+                console.log(result);
+                reg.showNotification('Hola Mundo');
+            });
+        });
+
+
 }
+
+/*fetch('https://reqres.in/api/users')
+    .then(response => response.text())
+    .then(console.log);
+*/
+
+//if(window.SyncManager) {}
--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@ self.addEventListener('install', event => {
             console.log('SW: Instalaciones terminadas.');
             self.skipWaiting();
             resolve();
-        }, 1000);        
+        }, 1);        
     });
 
     // Esperar hasta que la promesa haya terminado: 
@@ -26,4 +26,29 @@ self.addEventListener('install', event => {
 self.addEventListener('activate', event => {
     // Se usa para borrar caché viejo.
     console.log('SW: Activo, listo para controlar la app.');
+});
+
+// FETCH: Manejo de peticiones HTTP
+self.addEventListener('fetch', event => {
+    // Aplicar estrategias del caché
+    /*   
+    console.log('SW: ', event.request.url);
+    if(event.request.url.includes('https://reqres.in')) {
+        const respuesta = new Response(`{ok: false, mensaje: 'mi perrito caliente'}`);
+        event.respondWith(respuesta);
+    }
+    */
+});
+
+// SYNC: Recuperamos la conexión a internet y hace una tarea especifica.
+self.addEventListener('sync', event => {
+    console.log('Tenemos conexión!');
+    console.log(event);
+    // tag marca un evento con algun nombre
+    console.log(event.tag);
+});
+
+// PUSH: Manejar las push notifications
+self.addEventListener('push', event => {
+    console.log('Notificacion recibida');
 });
